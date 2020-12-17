3eb2f0ab184b61c0c0d1a3b6440d2347eff32ebe
Mehedi Hasan <mehedi.hasan@appscode.com>
2020-12-17 10:26:13 +0600

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/heheh13/testmod
+
+go 1.15
---
+++ hello.go
@@ -0,0 +1,8 @@
+package testmod
+
+import "fmt"
+
+// Hi is a function to say hi
+func Hi(name string) string {
+	return fmt.Sprintf("hi %s", name)
+}
